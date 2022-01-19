831104ca0b64ccc2f7a9311575b9b9c73f7e09b0
ZjeniiaSt <zeniiastupjak@gmail.com>
2022-01-19 19:35:59 +0300

    fetch

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,31 @@
-function Home() {
-  return <h2>HOME</h2>;
-}
+import { useState, useEffect } from 'react';
+import { getTrendingMovies } from '../services/apiServise';
 
+function Home() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const movies = await getTrendingMovies();
+      console.log(movies);
+      setTrendingMovies(movies);
+    };
+    fetch();
+  }, []);
+
+  return (
+    <ul>
+      {trendingMovies.map(move => (
+        <li key={move.id}>
+          <p>{move.title}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${move.poster_path}`}
+            width="250px"
+            alt={move.title}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
 export default Home;
---
+++ src/components/services/apiServise.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_KEY = '07518c21ec464688cc3129545b5c13db';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = { api_key: API_KEY };
+
+async function getTrendingMovies() {
+  const response = await axios.get(`trending/movie/day?`);
+  return response.data.results;
+}
+
+export { getTrendingMovies };
